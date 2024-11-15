f6c69f54eb57b1f94d3c2a61fd6f2bd940a5d3c7
Pxndxs 🐼 <pxnditxyr@gmail.com>
2024-11-15 13:17:20 -0400

    chore(seed): remove a user from the user seed data 🗑️👤🚫

--- db/seed-data/users.ts
+++ db/seed-data/users.ts
@@ -4,14 +4,6 @@ import bcryptjs from 'bcryptjs'
 export const seedUsers = [
   {
     id: UUID(),
-    name: 'Pxndxs 🐼',
-    lastName: 'Ricaldi',
-    email: 'pxndxs@jobsnow.com',
-    password: bcryptjs.hashSync( '123456', 10 ),
-    roleId: 'admin',
-  },
-  {
-    id: UUID(),
     name: 'Admin',
     lastName: 'Administrador',
     email: 'admin@jobsnow.com',
