a4f53524c637bcfc86a715aed9369027752adad1
Pxndxs 🐼 <pxnditxyr@gmail.com>
2024-11-24 03:57:30 -0400

    feat(worker-filter): complete worker filtering functionality on the homepage 🔍👷🖥️
    feat(worker-search): implement action for searching and filtering worker profiles 🛠️📋🌟

--- src/actions/worker-profiles/findAll.ts
+++ src/actions/worker-profiles/findAll.ts
@@ -1,29 +1,26 @@
 import { defineAction } from 'astro:actions'
-import { db, desc, eq, like, or, ReviewWorker, User, WorkerProfile } from 'astro:db'
+import { and, CommentService, db, desc, eq, like, or, ReviewWorker, Service, ServiceCategory, User, WorkerProfile } from 'astro:db'
 import { z } from 'astro:schema'
 
 export const findAllWorkerProfiles = defineAction({
   accept: 'json',
   input: z.object({
     search: z.string().optional(),
-    calificacion: z.string().optional(),
+    rating: z.number().optional(),
     serviceCategoryId: z.string().optional(),
   }),
-  handler: async ({ search, calificacion, serviceCategoryId }) => {
+  handler: async ({ search, rating, serviceCategoryId }) => {
+    let searchQuery = search ?? ''
+    searchQuery = '%' + searchQuery + '%'
 
-    const data = await db
+    const workerProfiles = await db
       .select()
       .from( WorkerProfile )
       .innerJoin( User, eq( User.id, WorkerProfile.userId ) )
       .where(
         or(
-          or(
-            like( User.name, search ?? '%%' ),
-            like( User.lastName, search ?? '%%' ),
-            like( WorkerProfile.description, search ?? '%%' ),
-            like( WorkerProfile.experience, search ?? '%%' ),
-            like( WorkerProfile.skills, search ?? '%%' ),
-          ),
+          like( User.name, searchQuery ),
+          like( User.lastName, searchQuery ),
         ),
       )
       .orderBy(
@@ -32,13 +29,58 @@ export const findAllWorkerProfiles = defineAction({
         )
        )
 
+    let workerProfilesFiltered = await Promise.all(workerProfiles.map(async ({ WorkerProfile, User }) => {
 
+      const publications = await db
+        .select()
+        .from(Service)
+        .innerJoin(CommentService, eq(Service.id, CommentService.serviceId))
+        .innerJoin(ServiceCategory, eq(Service.categoryId, ServiceCategory.id))
+        .where(
+          and(
+            eq(Service.workerId, WorkerProfile.id),
+            eq(Service.status, true),
+            eq(CommentService.status, true),
+          )
+        )
+        .orderBy(
+          desc(
+            Service.createdAt
+          )
+        )
+
+      let ratingAverage = 0
+      let noRepeatedServiceCategoryIds : any = []
+
+      if (publications.length > 0) {
+        const ratingSum = publications.reduce((total, publication) => {
+          return total + publication.CommentService.rating
+        }, 0)
+
+        ratingAverage = ratingSum / publications.length
+
+        const serviceCategoryIds = publications.map(ps => ps.ServiceCategory.id)
+        noRepeatedServiceCategoryIds = Array.from(new Set(serviceCategoryIds))
+      }
+
+      return {
+        ...WorkerProfile,
+        user: User,
+        rating: ratingAverage,
+        serviceCategories: noRepeatedServiceCategoryIds
+      }
+    }))
+
+    if ( rating !== undefined && rating !== 0 ) {
+      workerProfilesFiltered = workerProfilesFiltered.filter(wp => wp.rating >= rating)
+    }
+
+    if ( serviceCategoryId !== undefined && serviceCategoryId !== '' ) {
+      workerProfilesFiltered = workerProfilesFiltered.filter(wp => wp.serviceCategories.includes(serviceCategoryId))
+    }
 
     return {
-      workerProfiles: data.map( ( { WorkerProfile, User } ) => ( {
-        ...WorkerProfile,
-        user: User
-      } ) )
+      workerProfiles: workerProfilesFiltered
     }
   }
 })
