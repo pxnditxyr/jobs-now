aa6e1e59809bedb77c8464e81df51b6a6c8e62e8
Pxndxs 🐼 <pxnditxyr@gmail.com>
2024-12-01 22:33:33 -0400

    fix(chat): resolve issue when creating a new conversation by checking for existing ones 🔄💬🔧
    style(ui): apply additional UI improvements for better user experience 🎨✨🖥️

--- src/actions/chats/create-conversation.action.ts
+++ src/actions/chats/create-conversation.action.ts
@@ -20,6 +20,8 @@ export const createConversation = defineAction({
   } ),
   handler: async ({ isGroup, name, participantIds }) => {
     try {
+      console.log({ participantIds })
+
       const participantsUser1 = await db
         .select()
         .from( ConversationParticipant )
@@ -30,6 +32,8 @@ export const createConversation = defineAction({
           )
         )
 
+      console.log({ participantsUser1 })
+
       const participantsUser2 = await db
         .select()
         .from( ConversationParticipant )
@@ -39,49 +43,44 @@ export const createConversation = defineAction({
             eq( ConversationParticipant.status, true ),
           )
         )
+      console.log({ participantsUser2 })
 
       const participantsUser1Id = participantsUser1.map( p => p.conversationId )
       const participantsUser2Id = participantsUser2.map( p => p.conversationId )
-      const setParticipantsUser1Id = new Set( participantsUser1Id )
 
-      if ( participantsUser1Id.length !== 0 && participantsUser2Id.length !== 0 ) {
-        const existingConversation = participantsUser1Id.some( id => setParticipantsUser1Id.has( id ) )
-        if ( existingConversation ) {
-          const [ conversation ] = await db
-            .select()
-            .from( Conversation )
-            .where(
-              eq( Conversation.id, participantsUser2Id[ 0 ] )
-            )
-          return {
-            success: true,
-            conversationId: conversation.id,
-          }
-        }
+      const commonConversationIds = participantsUser1Id.filter( id => participantsUser2Id.includes( id ) )
+
+      if ( commonConversationIds.length > 0 ) {
+        const conversationId = commonConversationIds[ 0 ]
+        return {
+          success: true,
+          conversationId,
+        };
       }
 
+      // Si no existe, crear una nueva conversación
       const conversationId = UUID()
 
-      await db.insert( Conversation ).values({
+      await db.insert(Conversation).values({
         id: conversationId,
         isGroup,
         name: isGroup ? name?.trim() : null,
         createdAt: new Date(),
         updatedAt: new Date(),
-      })
+      });
 
-      const participants = participantIds.map( userId => ({
+      const participants = participantIds.map(userId => ({
         id: UUID(),
         conversationId,
         userId,
         joinedAt: new Date(),
         role: isGroup ? 'member' : 'participant',
         status: true,
-      }) )
+      }));
 
-      await db.insert( ConversationParticipant ).values(participants)
+      await db.insert(ConversationParticipant).values(participants);
 
-      return { success: true, conversationId }
+      return { success: true, conversationId };
     } catch ( error: any ) {
       throw new Error( error )
     }
